edf06cc2e4142e5a3dcef64d29ec278a10995e3d
RCastillo <rafael.castillo@utec.edu.pe>
2026-03-25 15:21:18 -0500

    Added new files generated by CMake in the build directory. These files include CMake cache, build files, and API query/reply files. This commit is necessary to keep track of the build configuration and ensure that the project can be built correctly in different environments.

---
+++ build/CMakeFiles/Proj_1.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/casti/UTEC_CollegeProjects/UTEC_3rdPeriod/Programacion3/Semana 1/Proj_1/Calumno.cpp" "CMakeFiles/Proj_1.dir/Calumno.cpp.obj" "gcc" "CMakeFiles/Proj_1.dir/Calumno.cpp.obj.d"
+  "C:/Users/casti/UTEC_CollegeProjects/UTEC_3rdPeriod/Programacion3/Semana 1/Proj_1/main.cpp" "CMakeFiles/Proj_1.dir/main.cpp.obj" "gcc" "CMakeFiles/Proj_1.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
